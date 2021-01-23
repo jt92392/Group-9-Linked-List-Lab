89ebb4a8f7965538fb9c98c1015e1ef37b6167b0
jtknutse <77871170+jtknutse@users.noreply.github.com>
2021-01-22 23:03:13 -0700

    Update AdvRoundRobinSimulation.java

--- AdvRoundRobinSimulation.java
+++ AdvRoundRobinSimulation.java
@@ -2,9 +2,16 @@
 public class AdvRoundRobinSimulator {
     // Constants
     
+    // Allocate a new CircularlyLinkedList<Process> and return a reference to it after populating
+    // it with the processes from the input instruction file "ProcessSequence1.txt"
+    public static CircularlyLinkedList<Process> getProcessesFromFile(String filename) {
+        
+    }
+    
     // Reads the inputs in the format AT <time> ADD <process-name> <duration> 
     //and schedules the processes in 10ms time slices
     public static void main(String[] args) {
+        CircularlyLinkedList<Process> processList = getProcessesFromFile("ProcessSequence1.txt");
         
     }
 }
